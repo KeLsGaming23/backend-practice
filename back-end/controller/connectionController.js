ef1e93ebc6b5c91c24bd9ba10912414d18f42df1
KelsGaming <alcorizamichael@gmail.com>
2025-03-16 15:23:34 +0800

    Create connectionController to test database credentials

---
+++ back-end/controller/connectionController.js
@@ -0,0 +1,18 @@
+import connectionPool from "../config/connectionPool";
+
+export const connectionController = (req, res) => {
+
+    connectionPool.getConnection((err, connection) => {
+
+        if(err){
+            console.error("Database connection failed:", err.message)
+            return res.status(500).json({ message: "Database connection failed", error: err.message});
+        }
+
+        console.log("Database connection verified");
+        connection.release();
+        res.json({ message: "Connection to database is successful"});
+
+    });
+
+};
