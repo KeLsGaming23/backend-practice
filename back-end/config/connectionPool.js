a396cb8e30f12f15ec828e951ad5e42094ab4606
KelsGaming <alcorizamichael@gmail.com>
2025-03-16 15:16:27 +0800

    Create config connectionPool for MYSQL database and env file for MYSQL remote Credentails

---
+++ back-end/config/connectionPool.js
@@ -0,0 +1,27 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
+//Create connection pool
+const connectionPool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
+});
+//test database connection
+connectionPool.getConnection((err, connection) => {
+
+    if(err){
+        console.error("Database connection failed on startup: ", err.message)
+        return;
+    }
+
+    console.log("Connected to MYSQL successfully:");
+    connection.release();
+
+});
+
+export default connectionPool;
