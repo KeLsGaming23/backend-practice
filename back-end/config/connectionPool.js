c606cdddc8b16cc1cac3c10908a9a0a819d2230f
KelsGaming <alcorizamichael@gmail.com>
2025-03-17 11:44:57 +0800

    Add Database in connectionPool to  select the default database

--- back-end/config/connectionPool.js
+++ back-end/config/connectionPool.js
@@ -8,6 +8,7 @@ const connectionPool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     port: 3306
 });
 
