8a116e8cc7388ef6abccbbdf48f1e06133acf2f4
KelsGaming <alcorizamichael@gmail.com>
2025-03-18 04:47:47 +0800

    Create simple POST to store data

---
+++ back-end/controller/postController.js
@@ -0,0 +1,23 @@
+import connectionPool from "../config/connectionPool.js";
+
+
+const postController = async (req, res) => {
+    try {
+        console.log("Received body:", req.body); // Debugging log
+        const { title, description } = req.body;
+
+        //validate
+        if (!title || !description) {
+            return res.status(400).json({ message: "Title and Description is required" });
+        }
+        const insertIntoQuery = "INSERT INTO post_entry (title, description, date_created) VALUES (?, ?, NOW())";
+        const [result] = await connectionPool.promise().query(insertIntoQuery, [title, description]);
+
+        res.status(201).json({ message: "Post created successfully", postId: result.insertId });
+    } catch (error) {
+        console.error("Error creating post:", error);
+        res.status(500).json({ message: "Server Error" });
+    }
+}
+
+export { postController }
--- back-end/index.js
+++ back-end/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import defaultRoute from './route/defaultRoute.js';
+import postRoute from './route/postRoute.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/', defaultRoute)
+app.use('/', defaultRoute);
+app.use('/api', postRoute);
 
 
 app.listen(8800, ()=>{
