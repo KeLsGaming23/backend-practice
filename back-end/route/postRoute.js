814e75f4119ff8a44766a4ef9f0dd1376664c558
KelsGaming <alcorizamichael@gmail.com>
2025-03-20 05:09:26 +0800

    Practice complete

--- back-end/route/postRoute.js
+++ back-end/route/postRoute.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import postController from '../controller/postController.js';
-import { registerUserController } from '../controller/userController.js';
+import { loginUserController, registerUserController } from '../controller/userController.js';
 
 const postRoute = express.Router();
 
 postRoute.post('/create-post-entry', postController);
 postRoute.post('/create-new-user', registerUserController);
-
+postRoute.post('/login', loginUserController);
 export default postRoute;
