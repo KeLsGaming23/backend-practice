782b8fe094717bc318c8010f6f961638acda8473
KelsGaming <alcorizamichael@gmail.com>
2025-03-20 04:58:35 +0800

    Practice complete

--- back-end/controller/userController.js
+++ back-end/controller/userController.js
@@ -34,4 +34,37 @@ const registerUserController = async (req, res) => {
     }
 }
 
-export {registerUserController}
+const loginUserController = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(404).json({message: "All Fields are required!"});
+        }
+        const userEmailQuery = "SELECT * FROM user_data WHERE email = ?";
+        const [userEmail] = await connectionPool.promise().query(userEmailQuery, [email]);
+
+        if(userEmail.length === 0){
+            return res.status(404).json({message: "Invalid email"});
+        }
+
+        const user = userEmail[0];
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(404).json({message: "Invalid Password"});
+        }
+
+        const token = jwt.sign(
+            {userId: user.id, username: user.username, email: user.email},
+            process.env.JWT_SECRET,
+            {expiresIn: '1h'}
+        );
+
+        res.status(200).json({message: `Welcome ${user.username} with email: ${user.email}`, token});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Server Error"});
+    }
+}
+
+export {registerUserController, loginUserController}
--- back-end/route/postRoute.js
+++ back-end/route/postRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import postController from '../controller/postController.js';
-import { registerUserController } from '../controller/userController.js';
+import { loginUserController, registerUserController } from '../controller/userController.js';
 
 const postRoute = express.Router();
 
 postRoute.post('/create-post-entry', postController);
 postRoute.post('/create-new-user', registerUserController);
+postRoute.post('/login', loginUserController);
 
 export default postRoute;
