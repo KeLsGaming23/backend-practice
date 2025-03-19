1d6151f9c7a10bcf09968ba772eb4cb319d9785e
KelsGaming <alcorizamichael@gmail.com>
2025-03-19 21:06:36 +0800

    Create new post method to register new user with hashpassword

---
+++ back-end/controller/userController.js
@@ -0,0 +1,37 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import connectionPool from '../config/connectionPool.js';
+
+const registerUserController = async (req, res) => {
+    try {
+        const {username, email, password} = req.body;
+        if(!username || !email || !password){
+            return json.status(404).json({ message: "All fields are required" });
+        }
+        const checkEmailQuery = "SELECT * FROM user_data WHERE email = ?";
+        const checkUserNameQuery = "SELECT * FROM user_data WHERE username = ?";
+
+        const [existingEmail] = await connectionPool.promise().query(checkEmailQuery, [email]);
+        const [existingUserName] = await connectionPool.promise().query(checkUserNameQuery, [username]);
+
+        if(existingEmail.length > 0){
+            return res.status(404).json({ message: "Email already registered!!" });
+        }
+        if(existingUserName.length > 0){
+            return res.status(404).json({ message: "Username already exist!" });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const insertUserQuery = "INSERT INTO user_data (username, email, password, date_created) VALUES (?, ?, ?, NOW())";
+        await connectionPool.promise().query(insertUserQuery, [username, email, hashedPassword]);
+
+        res.status(200).json({message: `Username: ${username} Successfully Registered`});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server Error" });
+    }
+}
+
+export {registerUserController}
--- back-end/route/postRoute.js
+++ back-end/route/postRoute.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import postController from '../controller/postController.js';
+import { registerUserController } from '../controller/userController.js';
 
 const postRoute = express.Router();
 
 postRoute.post('/create-post-entry', postController);
+postRoute.post('/create-new-user', registerUserController);
 
 export default postRoute;
